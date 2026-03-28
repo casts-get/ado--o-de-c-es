b7dd80d2502da01201bb78e51beeca0e897f78bb
carine_dos-santos@estudante.sesisenai.org.br <carine_dos-santos@estudante.sesisenai.i>
2026-03-27 21:55:05 -0300

    add-adicionado um if para detectar erros no formulario e corrigir se algo estiver fora das regras

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
 const msg = document.getElementById("mensagem");
 
-form.addEventListener("submit", function(e)) {
+form.addEventListener("submit", function(e) {
     e.preventDefault();
     msg.innerHTML = "";
 
@@ -17,4 +17,52 @@ form.addEventListener("submit", function(e)) {
     let horas = document.getElementById("horas").value;
     let motivo = document.getElementById("motivo").value;
     let termo = document.getElementById("termo").checked;
+ 
+ 
+    if (nome.length < 3) return erro("Nome inválido");
+    if (!email.includes("@")) return erro("Email inválido");
+    if (telefone.length < 8) return erro("Telefone inválido");
+    if (!cpf) return erro("CPF obrigatório");
+    if (cpfsCadastrados.includes(cpf)) return erro("CPF já cadastrado");
+    if (idade < 18) return erro("Precisa ser maior de 18");
+    if (!cidade) return erro("Cidade obrigatória");
+    if (!moradia) return erro("Escolha a moradia");
+    if (!quintal) return erro("Informe sobre quintal");
+    if (!pet) return erro("Informe se já teve pet");
+    if (isNaN(horas)) return erro("Horas inválidas");
+    if (motivo.length < 10) return erro("Motivo muito curto");
+    if (!termo) return erro("Aceite o termo");
+
+
+
+    if (moradia === "apartamento") {
+        alert("Verificar se o local permite animais");
+    }
+
+    if (moradia === "casa" && quintal === "nao") {
+        alert("Casa sem quintal pode não ser adequada");
+    }
+
+    if (moradia === "apartamento" && quintal === "sim") {
+        return erro("Apartamento não pode ter quintal");
+    }
+
+    if (horas > 8) {
+        alert("Animal ficará muito tempo sozinho");
+    }
+
+    if (pet === "nao") {
+        alert("A ONG poderá acompanhar sua adaptação");
+    }
+
+    if (motivo.toLowerCase() === "quero" || motivo.toLowerCase() === "porque sim") {
+        return erro("Motivo genérico não permitido");
+    }
+
+    alert("Formulário enviado com sucesso!");
+
+});
+
+function erro(texto) {
+    msg.innerHTML = texto;
 }
